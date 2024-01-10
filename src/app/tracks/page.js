818d336feeede43444732ca6a9a86451114df9a9
Irwing Naranjo <khalisser@gmail.com>
2024-01-10 10:09:18 -0300

    error tracks

--- src/app/tracks/page.js
+++ src/app/tracks/page.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import MainLayout from '@/components/MainLayout'
-import { getTracks } from '@/services/tracks.service'
+// import { getTracks } from '@/services/tracks.service'
 
 const TracksPage = async () => {
   // const tracks = await getTracks()

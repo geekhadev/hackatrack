81185f49c90c5565f18c7dd923678d13b77c6e75
Irwing Naranjo <khalisser@gmail.com>
2024-01-10 10:06:21 -0300

    fix url api

--- src/services/tracks.service.js
+++ src/services/tracks.service.js
@@ -1,11 +1,11 @@
 export const getTracks = async () => {
-  const response = await fetch('hacka-six.vercel.app/api/tracks')
+  const response = await fetch('https://hacka-six.vercel.app/api/tracks')
   const tracks = await response.json()
   return tracks
 }
 
 export const findTracks = async (id) => {
-  const response = await fetch('hacka-six.vercel.app/api/tracks/' + id)
+  const response = await fetch('https://hacka-six.vercel.app/api/tracks/' + id)
   const tracks = await response.json()
   return tracks
 }

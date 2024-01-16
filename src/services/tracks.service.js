6e300c18cf1e0306488aa2ebc0763c509c232b46
Irwing Naranjo <khalisser@gmail.com>
2024-01-15 23:53:13 -0300

    fix: name enviroment api var

--- src/services/tracks.service.js
+++ src/services/tracks.service.js
@@ -1,5 +1,5 @@
 export const getTracks = async () => {
-  const response = await fetch(`${process.env.API}/tracks`)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API}/tracks`)
   const tracks = await response.json()
   return tracks
 }

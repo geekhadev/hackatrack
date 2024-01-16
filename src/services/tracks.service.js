2a8c8762eb2adb5957f3693bd136604fa28a7bd9
Irwing Naranjo <khalisser@gmail.com>
2024-01-15 23:45:08 -0300

    add enviroments

--- src/services/tracks.service.js
+++ src/services/tracks.service.js
@@ -1,11 +1,5 @@
 export const getTracks = async () => {
-  const response = await fetch('https://hacka-six.vercel.app/api/tracks')
-  const tracks = await response.json()
-  return tracks
-}
-
-export const findTracks = async (id) => {
-  const response = await fetch('https://hacka-six.vercel.app/api/tracks/' + id)
+  const response = await fetch(`${process.env.API}/tracks`)
   const tracks = await response.json()
   return tracks
 }

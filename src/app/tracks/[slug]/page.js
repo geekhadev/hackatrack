5903cea8184865bd312cde8c14283f0a9e99e9c3
Irwing Naranjo <khalisser@gmail.com>
2024-01-16 15:15:29 -0300

    fix rendering vercel

--- src/app/tracks/[slug]/page.js
+++ src/app/tracks/[slug]/page.js
@@ -1,9 +1,22 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 import { marked } from 'marked'
 import TrackLayout from '@/components/TrackLayout'
-import { findTrackFetch } from '@/services/tracks.service'
+
+const getArticle = async (slug) => {
+  const directoryPath = path.join(process.cwd(), './src/content')
+  const filePath = path.join(directoryPath, slug + '.md')
+
+  const markdownWithMeta = fs.readFileSync(filePath, 'utf-8')
+
+  const { data: frontmatter, content } = matter(markdownWithMeta)
+
+  return { frontmatter, content }
+}
 
 export default async function Page ({ params }) {
-  const { frontmatter, content } = await findTrackFetch(params.slug)
+  const { frontmatter, content } = await getArticle(params.slug)
   return (
     <TrackLayout>
         <h1>{frontmatter.title}</h1>

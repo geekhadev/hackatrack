026e895bb7d5fab93bf175979f1ef911a40457ae
Irwing Naranjo <khalisser@gmail.com>
2024-02-05 15:55:16 -0300

    fix semicolons

---
+++ public/content/deploy-docker-javascript-projects.md
@@ -0,0 +1,54 @@
+---
+title: 'Despliegue de proyectos JavaScript con Docker'
+date: '13 Enero 2024'
+excerpt: 'Docker para distribuir aplicaciones JavaScript en cualquier entorno ...'
+cover: '/tracks/deploy-docker-javascript-projects.png'
+deploy: ''
+github: ''
+youtube: ''
+authorName: '@geekhadev'
+authorAvatar: 'https://avatars.githubusercontent.com/u/499907?v=4'
+authorGithub: 'https://github.com/geekhadev'
+status: 'published'
+---
+
+### Introducción
+
+<br />
+
+### Requisitos
+
+<br />
+
+### Estructura del proyecto
+
+```
+your-project/
+  ├── node_modules/
+  ├── public/
+  │   ├── next.svg
+  │   └── vercel.svg
+  ├── src/
+  │   └── app/
+  │       ├── page.js
+  │       ├── layout.js
+  │       ├── global.css
+  │       ├── favicon.ico
+  │       └── api/
+  │           └── check.js
+  ├── .eslintrc.json
+  ├── .gitignore
+  ├── jsconfig.json
+  ├── next.config.js
+  ├── package-lock.json
+  ├── package.json
+  ├── postcss.config.js
+  ├── README.md
+  └── tailwind.config.js
+```
+
+<br />
+
+### Recomendaciones
+
+### Comunidad
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,40 +1,40 @@
-import "./globals.css";
-import Header from "@/components/Header";
+import './globals.css'
+import Header from '@/components/Header'
 
 export const metadata = {
-  title: "Hack a Track",
+  title: 'Hack a Track',
   description:
-    "Plataforma web con la información de los tracks educativos de HACK A BOSS",
+    'Plataforma web con la información de los tracks educativos de HACK A BOSS',
   openGraph: {
-    title: "Hack a Track",
+    title: 'Hack a Track',
     description:
-      "Plataforma web con la información de los tracks educativos de HACK A BOSS",
-    url: "https://hackatrack.vercel.app/",
+      'Plataforma web con la información de los tracks educativos de HACK A BOSS',
+    url: 'https://hackatrack.vercel.app/',
     images: [
       {
-        url: "/cover.png",
-        alt: "Hack a Track",
-      },
+        url: '/cover.png',
+        alt: 'Hack a Track'
+      }
     ],
-    locale: "es_ES",
-    type: "website",
+    locale: 'es_ES',
+    type: 'website'
   },
   twitter: {
-    card: "summary_large_image",
-    title: "Hack a Track",
+    card: 'summary_large_image',
+    title: 'Hack a Track',
     description:
-      "Plataforma web con la información de los tracks educativos de HACK A BOSS",
-    url: "https://hackatrack.vercel.app/",
+      'Plataforma web con la información de los tracks educativos de HACK A BOSS',
+    url: 'https://hackatrack.vercel.app/',
     images: [
       {
-        url: "/cover.png",
-        alt: "Hack a Track",
-      },
-    ],
-  },
-};
+        url: '/cover.png',
+        alt: 'Hack a Track'
+      }
+    ]
+  }
+}
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({ children }) {
   return (
     <html lang="en">
       <body className="bg-[#121212]">
@@ -42,5 +42,5 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
-  );
+  )
 }
